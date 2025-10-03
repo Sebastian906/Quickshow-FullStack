eae16e8e3e5f55c998477889053f990061063c4d
Sebastian906 <elsebas1912@gmail.com>
2025-10-03 15:09:10 -0500

    Se crea adorno en la interfaz de películas destacadas para el botón de ver todas las películas

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,5 +1,6 @@
 import { ArrowRight } from "lucide-react"
 import { useNavigate } from "react-router-dom"
+import BlurCircle from "./BlurCircle";
 
 const FeaturedSection = () => {
 
@@ -8,10 +9,11 @@ const FeaturedSection = () => {
     return (
         <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
             <div className="relative flex items-center justify-between pt-20 pb-10">
+                <BlurCircle top="0" right="-80px"/>
                 <p className="text-gray-300 font-medium text-lg">Now Showing</p>
                 <button 
                     onClick={() => navigate('/movies')}
-                    className="group flex items-center gap-2 text-sm text-gray-300"
+                    className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer"
                 >
                     View All 
                     <ArrowRight 
