defe0b815d44a45578646ffb95eb052f36bff839
Sebastian906 <elsebas1912@gmail.com>
2025-10-05 15:53:55 -0500

    Integración de sección de traileres y vista previas de trailers en youtube

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -16,20 +16,20 @@ export const assets = {
 
 export const dummyTrailers = [
     {
-        image: "https://img.youtube.com/vi/WpW36ldAqnM/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=WpW36ldAqnM'
+        image: "https://img.youtube.com/vi/kQUPdVxZNPk/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/embed/kQUPdVxZNPk'
     },
     {
-        image: "https://img.youtube.com/vi/-sAOWhvheK8/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=-sAOWhvheK8'
+        image: "https://img.youtube.com/vi/Ox8ZLF6cGM0/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/embed/Ox8ZLF6cGM0'
     },
     {
-        image: "https://img.youtube.com/vi/1pHDWnXmK7Y/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=1pHDWnXmK7Y'
+        image: "https://img.youtube.com/vi/OpThntO9ixc/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/embed/OpThntO9ixc'
     },
     {
-        image: "https://img.youtube.com/vi/umiKiW4En9g/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=umiKiW4En9g'
+        image: "https://img.youtube.com/vi/VWqJifMMgZE/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/embed/VWqJifMMgZE'
     },
 ]
 
