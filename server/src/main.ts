6c5baa670ce2aef7e31aa1de817f4c16bd2c9955
Sebastian906 <elsebas1912@gmail.com>
2025-10-16 21:40:54 -0500

    Configuración inicial para iniciar servidor en el backend e instalación de paquetes necesarios

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilitar CORS
+  app.enableCors();
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+  console.log(`Server listening at http://localhost:${port}`)
 }
 bootstrap();
