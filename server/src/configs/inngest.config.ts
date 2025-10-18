17d45e3ed42e23f91f26ac613d6714a42259d0e1
Sebastian906 <elsebas1912@gmail.com>
2025-10-18 14:41:01 -0500

    Se añade la signingKey en el config de inngest

--- server/src/configs/inngest.config.ts
+++ server/src/configs/inngest.config.ts
@@ -3,4 +3,5 @@ import { Inngest } from 'inngest';
 export const inngest = new Inngest({
     id: 'Quickshow',
     eventKey: process.env.INGEST_EVENT_KEY,
+    signingKey: process.env.INNGEST_SIGNING_KEY,
 });
