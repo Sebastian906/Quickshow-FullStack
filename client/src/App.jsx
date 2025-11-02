2ba49be98c9e4164e54cfc973ef0adb2a89acd73
Sebastian906 <elsebas1912@gmail.com>
2025-11-02 12:04:45 -0500

    Configuración de componentes en Loading para cargar en lace de pagos con Stripe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import ListBookings from './pages/admin/ListBookings'
 import { useAppContext } from './context/AppContext'
 import { SignIn } from '@clerk/clerk-react'
 import AdminProtectedRoute from './components/admin/AdminProtectedRoute'
+import Loading from './components/Loading'
 
 const App = () => {
 
@@ -33,6 +34,7 @@ const App = () => {
         <Route path='/movies/:id' element={<MovieDetails/>}/>
         <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
         <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/loading/:nextUrl' element={<Loading/>}/>
         <Route path='/favorites' element={<Favorite/>}/>
         <Route path='/admin/*' element={
           <AdminProtectedRoute>
